3133ddb47e19d974e8f8c6f01737d624c65a45e3
Enrique <enrangel@csumb.edu>
2024-10-31 11:38:20 -0700

    Fixing errors

---
+++ src/components/Profile.js
@@ -0,0 +1,11 @@
+import React, { useState } from 'react';
+import Navbar from './Navbar';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+const Profile = () => {
+    return (
+<Navbar/>
+    );
+};
+
+export default Profile;
