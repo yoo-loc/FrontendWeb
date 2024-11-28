f821742adeef53152ccde14ce854a9754e6f35df
LRuizCapistran <LRuizCapistran>
2024-11-27 22:03:46 -0800

    Added comment editing functionality with last edited time

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -18,12 +18,26 @@ const Navbar = ({ onLogout, isAuthenticated }) => {
 
                 {isDropdownOpen && (
                     <div className="dropdown">
+                        {/* Links for Fully Worked Routes */}
                         <Link to="/dashboard" className="dropdown-link">Dashboard</Link>
                         <Link to="/userFeed" className="dropdown-link">Feed</Link>
-                        <Link to="/FavoriteRecipes" className="dropdown-link">Favorite Recipes</Link>
+                        <Link to="/favoriteRecipes" className="dropdown-link">Favorite Recipes</Link>
                         <Link to="/recipes" className="dropdown-link">Recipes</Link>
                         <Link to="/add-recipe" className="dropdown-link">Add Recipe</Link>
-                        <Link to="/logout" className="dropdown-link">Logout</Link>
+
+                        {/* Divider */}
+                        <div className="dropdown-divider"></div>
+
+                        {/* Unworked or Placeholder Links */}
+                        <div className="dropdown-unworked">
+                            <Link to="/profile" className="dropdown-link">Profile</Link>
+                            <Link to="/discover" className="dropdown-link">Discover Recipes</Link>
+                            <Link to="/share" className="dropdown-link">Share Recipes</Link>
+                            <Link to="/cook-together" className="dropdown-link">Cook Together</Link>
+                            <Link to="/logout" className="dropdown-link">Logout</Link>
+                            <Link to="/signup" className="dropdown-link">Sign Up</Link>
+                            <Link to="/login" className="dropdown-link">Login</Link>
+                        </div>
                     </div>
                 )}
             </div>
