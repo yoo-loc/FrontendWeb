c2030212bf7eeef0fa313d8c1fdc441ef8ea095b
LRuizCapistran <LRuizCapistran>
2024-11-30 22:34:59 -0800

    restored deleted page

--- src/components/RecipeDetail.js
+++ src/components/RecipeDetail.js
@@ -2,6 +2,11 @@ import React, { useEffect, useState } from 'react';
 import { useParams, useNavigate } from 'react-router-dom';
 import axios from 'axios';
 
+
+
+
+
+
 const RecipeDetail = () => {
     const { id } = useParams();
     const navigate = useNavigate();
