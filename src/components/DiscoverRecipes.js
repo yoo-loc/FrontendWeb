22de6fbd06750fcbaa5e074759f89d2ccef7adaa
Mason Allred <mallred@csumb.edu>
2024-12-10 03:51:29 -0800

    Updated styles of recipe pages

--- src/components/DiscoverRecipes.js
+++ src/components/DiscoverRecipes.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import axios from 'axios';
- // Add custom styling if needed
+import './DiscoverRecipes.css';
 
 const DiscoverRecipes = () => {
     const [recipes, setRecipes] = useState([]);
